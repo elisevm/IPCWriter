7085dc83ba4d8f1e60fca4bcf686c57f96fe4a10
tessatiu <gadjiew.rasim@gmail.com>
2023-10-01 09:34:30 +0300

    ipc writer

---
+++ cmake-build-debug/CMakeFiles/IPC_MSG_WRITER.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPC_MSG_WRITER.dir/main.cpp.o"
+  "IPC_MSG_WRITER"
+  "IPC_MSG_WRITER.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IPC_MSG_WRITER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/IPC_MSG_WRITER.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IPC_MSG_WRITER.
+# This may be replaced when dependencies are built.
